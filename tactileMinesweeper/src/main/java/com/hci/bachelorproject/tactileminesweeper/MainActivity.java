989549641b520dc6c146e450be27d9575ce3b26e
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2017-06-22 13:35:26 +0200

    changes to framework, minesweeper

--- tactileMinesweeper/src/main/java/com/hci/bachelorproject/tactileminesweeper/MainActivity.java
+++ tactileMinesweeper/src/main/java/com/hci/bachelorproject/tactileminesweeper/MainActivity.java
@@ -1,23 +1,17 @@
 package com.hci.bachelorproject.tactileminesweeper;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 import android.view.View;
-import android.widget.Toast;
 
 import com.hci.bachelorproject.webapplib.LinepodBaseActivity;
 
 public class MainActivity extends LinepodBaseActivity {
-    SpeechRecognizer speechRecognizer;
-    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         webAppUrl = "file:///android_asset/TactileMinesweeper/src/Android/index.html";
         super.onCreate(savedInstanceState);
+        webAppInterface.setOnTriggerSpeechCallback(this);
         webView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,19 +20,4 @@ public class MainActivity extends LinepodBaseActivity {
         });
     }
 
-
-    private void promptSpeechInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                "listening");
-        try {startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(this,
-                    "speech not supported",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
 }
